079b1e46ff363b17599781446c4aacf16801c719
Daniel Iheonu <iheonu6@gmail.com>
2025-03-11 17:39:44 +0100

    Updated src

---
+++ src/components/Search.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+export default function Search() {
+    const [query, setQuery] = useState("pizza");
+    // Synteax of the useEffect hook
+    useEffect(() => {
+        function demo() {
+            console.log("Demo function executed");
+        }
+        demo()
+    }, [query])
+    return <div>
+        <input value={query} onChange={(e) => setQuery(e.target.value)} type="text" />
+    </div>
+}
