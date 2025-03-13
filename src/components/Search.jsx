f52f60b7d591097b753c8ef9619ff84d9b5ce2b9
Daniel Iheonu <iheonu6@gmail.com>
2025-03-13 17:10:16 +0100

    my header styling

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,18 @@
 import { useEffect, useState } from "react";
 
-export default function Search() {
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "8a15d5fe04a747ed8b8f17f18a758450";
+export default function Search({ foodData,setFoodData }) {
     const [query, setQuery] = useState("pizza");
     // Synteax of the useEffect hook
     useEffect(() => {
-        function demo() {
-            console.log("Demo function executed");
+      async  function fetchFood(){
+          const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`)
+          const data = await res.json()
+          console.log(data.results)
+          setFoodData(data.results)
         }
-        demo()
+        fetchFood()
     }, [query])
     return <div>
         <input value={query} onChange={(e) => setQuery(e.target.value)} type="text" />
